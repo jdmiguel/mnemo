cef478edc5204f96960e349972a651d71d466e36
jdmiguel <jaimedmiguel@gmail.com>
2023-11-01 12:59:48 +0100

    remove .env.example and add check user id

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import { getServerAuthSession } from "@/server/auth";
 import AppLayout from "@/components/views/AppLayout";
 import MnemoHeader from "@/components/views/MnemoHeader";
+import { redirect } from "next/navigation";
 
-export default function DashboardPage() {
+export default async function DashboardPage() {
+  const session = await getServerAuthSession();
+
+  if (!session?.user.id) redirect("/signin");
+
   return (
     <AppLayout section="dashboard">
       <div className="flex flex-1 flex-col">
